95683741206a080b9380e0647dca16737f6fa6e6
samuelscheit <github@samuelscheit.com>
2026-05-06 05:58:51 +0200

    Use shared message array schema for mentions

--- src/api/routes/users/@me/mentions.ts
+++ src/api/routes/users/@me/mentions.ts
@@ -28,7 +28,7 @@ router.get(
     route({
         responses: {
             200: {
-                body: "MessageListResponse",
+                body: "APIMessageArray",
             },
             404: {
                 body: "APIErrorResponse",
