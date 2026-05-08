d204a7a1d09b769332f9f6293c7540c40f0c447a
samuelscheit <github@samuelscheit.com>
2026-05-08 14:55:05 +0200

    Tighten application type migration coverage

--- src/util/migration/postgres/1778207100000-ApplicationTypeInteger.ts
+++ src/util/migration/postgres/1778207100000-ApplicationTypeInteger.ts
@@ -1,13 +1,14 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
+const validApplicationTypeValues = ["1", "2", "3", "4"];
+
 export class ApplicationTypeInteger1778207100000 implements MigrationInterface {
     name = "ApplicationTypeInteger1778207100000";
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE applications ALTER COLUMN "type" TYPE integer USING CASE
             WHEN "type" IS NULL THEN NULL
-            WHEN jsonb_typeof("type") = 'number' THEN ("type" #>> '{}')::integer
-            WHEN jsonb_typeof("type") = 'string' AND ("type" #>> '{}') ~ '^[0-9]+$' THEN ("type" #>> '{}')::integer
+            WHEN jsonb_typeof("type") IN ('number', 'string') AND ("type" #>> '{}') IN (${validApplicationTypeValues.map((value) => `'${value}'`).join(", ")}) THEN ("type" #>> '{}')::integer
             ELSE NULL
         END;`);
     }
