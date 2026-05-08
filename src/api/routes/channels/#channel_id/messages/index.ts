dbb61fc4763d8a886e29832d3291d9538f366f50
samuelscheit <github@samuelscheit.com>
2026-05-08 05:40:31 +0200

    Remove stale message slowmode TODO

--- src/api/routes/channels/#channel_id/messages/index.ts
+++ src/api/routes/channels/#channel_id/messages/index.ts
@@ -236,7 +236,6 @@ export const messageUpload = multer({
  TODO: dynamically change limit of MessageCreateSchema with config
 
  https://discord.com/developers/docs/resources/channel#create-message
- TODO: text channel slowdown (per-user and across-users)
  Q: trim and replace message content and every embed field A: NO, given this cannot be implemented in E2EE channels
  TODO: only dispatch notifications for mentions denoted in allowed_mentions
 **/
