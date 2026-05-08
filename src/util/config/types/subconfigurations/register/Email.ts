5a0ff691f39beb1dcfc96e22ccc70723a90af156
samuelscheit <github@samuelscheit.com>
2026-05-08 05:28:50 +0200

    chore(config): remove stale email domain storage TODO

--- src/util/config/types/subconfigurations/register/Email.ts
+++ src/util/config/types/subconfigurations/register/Email.ts
@@ -20,6 +20,5 @@ export class RegistrationEmailConfiguration {
     required: boolean = false;
     allowlist: boolean = false;
     blocklist: boolean = true;
-    domains: string[] = []; // TODO: efficiently save domain blocklist in database
-    // domains: fs.readFileSync(__dirname + "/blockedEmailDomains.txt", { encoding: "utf8" }).split("\n"),
+    domains: string[] = [];
 }
