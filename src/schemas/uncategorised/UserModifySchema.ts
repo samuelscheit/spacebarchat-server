f02c283140d8c4a566533edb54f801308ea6a4a7
samuelscheit <github@samuelscheit.com>
2026-05-08 04:59:58 +0200

    Remove stale UserModifySchema TODO

--- src/schemas/uncategorised/UserModifySchema.ts
+++ src/schemas/uncategorised/UserModifySchema.ts
@@ -16,7 +16,6 @@
 	along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-// TODO: dedicated schemas?
 import { User_DisplayNameEffect, User_DisplayNameFont } from "discord-protos";
 
 export interface UserModifySchema {
