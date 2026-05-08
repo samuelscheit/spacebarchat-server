cecd071058fa485844222699f679b5927592b038
samuelscheit <github@samuelscheit.com>
2026-05-08 07:48:39 +0200

    Remove stale iframe modal callback TODO

--- src/api/routes/interactions/#interaction_id/#interaction_token/callback.ts
+++ src/api/routes/interactions/#interaction_id/#interaction_token/callback.ts
@@ -193,7 +193,6 @@ router.post(
                 // Deprecated
                 break;
             case InteractionCallbackType.IFRAME_MODAL:
-                // TODO
                 break;
             case InteractionCallbackType.LAUNCH_ACTIVITY:
                 // TODO
